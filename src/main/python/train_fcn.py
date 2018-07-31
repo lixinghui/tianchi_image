e1feab6647c90c8d1ab73bfed0b23199f4176e38
riverhxz <ravehun@qq.com>
2018-07-31 14:01:50 +0800

    model

--- src/main/python/train_fcn.py
+++ src/main/python/train_fcn.py
@@ -49,7 +49,7 @@ def _main():
     #model = multi_gpu_model(model,gpus=[0,1])
 
     logging = TensorBoard(log_dir=log_dir)
-    checkpoint = ModelCheckpoint(log_dir + '{arg.model:}ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
+    checkpoint = ModelCheckpoint(log_dir + args.model +'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
                                  monitor='val_loss', save_weights_only=True, save_best_only=True, period=3)
     reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, verbose=1)
     early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1)
