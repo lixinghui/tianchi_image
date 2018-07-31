2096c3038e8d4fed5f0893e590aa6e59dc94a41b
riverhxz <ravehun@qq.com>
2018-08-01 02:50:57 +0800

    ..

--- src/main/python/fcn/test_keras_mnist.py
+++ src/main/python/fcn/test_keras_mnist.py
@@ -6,11 +6,12 @@ Gets to 99.25% test accuracy after 12 epochs
 
 from __future__ import print_function
 import keras
+from keras.applications import VGG16
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K
+from keras import backend as K, Input, Model
 
 batch_size = 128
 num_classes = 10
@@ -43,18 +44,20 @@ print(x_test.shape[0], 'test samples')
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
-model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
+image_input = Input(shape=(img_rows, img_cols, 3))
+vgg = VGG16( include_top=False, weights=None, input_tensor=image_input)
+# model = Sequential()
+# model.add(Conv2D(32, kernel_size=(3, 3),
+#                  activation='relu',
+#                  input_shape=input_shape))
+# model.add(Conv2D(64, (3, 3), activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Dropout(0.25))
+x = Flatten()(vgg.output)
+x = Dense(128, activation='relu')(x)
+x = Dense(num_classes, activation='softmax')(x)
 
+model = Model([image_input], x )
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
